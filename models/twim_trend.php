adabf90792dcfd53386919a69f3c482bb709767c
nojimage <nojimage@gmail.com>
2011-03-20 20:57:16 +0900

    Add TwimTrend model

---
+++ models/twim_trend.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * for Trend API
+ *
+ * PHP versions 5
+ *
+ * Copyright 2011, nojimage (http://php-tips.com/)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @version   1.0
+ * @author    nojimage <nojimage at gmail.com>
+ * @copyright 2011 nojimage (http://php-tips.com/)
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link    　http://php-tips.com/
+ * @since   　File available since Release 1.0
+ * @see       http://dev.twitter.com/doc/get/trends
+ * @see       http://dev.twitter.com/doc/get/trends/current
+ * @see       http://dev.twitter.com/doc/get/trends/daily
+ * @see       http://dev.twitter.com/doc/get/trends/weekly
+ * @see       http://dev.twitter.com/doc/get/trends/available
+ * @see       http://dev.twitter.com/doc/get/trends/1
+ *
+ */
+class TwimTrend extends TwimAppModel {
+
+    /**
+     * Custom find types available on this model
+     *
+     * @var array
+     */
+    public $_findMethods = array(
+        'trends' => true,
+        'current' => true,
+        'daily' => true,
+        'weekly' => true,
+        'available' => true,
+        'woeid' => true,
+    );
+    /**
+     * The options allowed by each of the custom find types
+     *
+     * @var array
+     */
+    public $allowedFindOptions = array(
+        'trends' => array(),
+        'current' => array('exclude'),
+        'daily' => array('exclude', 'date'),
+        'weekly' => array('exclude', 'date'),
+        'available' => array('lat', 'long'),
+        'woeid' => array('woeid'),
+    );
+
+    /**
+     *
+     * @param mixed $type
+     * @param array $options
+     * @return array|false
+     */
+    public function find($type, $options = array()) {
+
+        if (method_exists($this, '_find' . Inflector::camelize($type))) {
+            return parent::find($type, $options);
+        }
+
+        if ($type === 'trends') {
+            $this->request['uri']['path'] = Inflector::underscore($type);
+        } else {
+            $this->request['uri']['path'] = '1/trends/' . Inflector::underscore($type);
+        }
+
+        if (array_key_exists($type, $this->allowedFindOptions)) {
+            $this->request['uri']['query'] = array_intersect_key($options, array_flip($this->allowedFindOptions[$type]));
+        }
+
+        return parent::find('all', $options);
+    }
+
+    /**
+     *
+     * @param string $state
+     * @param array $query
+     * @param array $results
+     * @return mixed
+     */
+    protected function _findWoeid($state, $query = array(), $results = array()) {
+        if ($state === 'before') {
+            if (empty($query['woeid'])) {
+                return false;
+            }
+            $this->request['uri']['path'] = '1/trends/' . $query['woeid'];
+            unset($query['woeid']);
+            $this->request['uri']['query'] = array_intersect_key($query, array_flip($this->allowedFindOptions['woeid']));
+            return $query;
+        } else if ($state === 'after') {
+            return !empty($results[0]) ? $results[0] : $results;
+        } else {
+            return $results;
+        }
+    }
+
+}
---
+++ tests/cases/models/twim_trend.test.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * test TwimTrend
+ *
+ * PHP versions 5
+ *
+ * Copyright 2011, nojimage (http://php-tips.com/)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @version   1.0
+ * @author    nojimage <nojimage at gmail.com>
+ * @copyright 2011 nojimage (http://php-tips.com/)
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link    　http://php-tips.com/
+ * @since   　File available since Release 1.0
+ *
+ */
+App::import('Model', 'Twim.TwimTrend');
+App::import('Datasource', array('Twim.TwimSource'));
+
+class TestTwimTrend extends TwimTrend {
+
+    public $alias = 'TwimTrend';
+    public $useDbConfig = 'test_twitter';
+
+}
+
+Mock::generatePartial('TwimSource', 'MockTwimTrendTwimSource', array('request'));
+
+/**
+ *
+ * @property TwimTrend $Trend
+ */
+class TwimTrendTestCase extends CakeTestCase {
+
+    public function startTest() {
+        ConnectionManager::create('test_twitter',
+                        array('datasource' => 'MockTwimTrendTwimSource'));
+
+        $this->Trend = ClassRegistry::init('Twim.TestTwimTrend');
+    }
+
+    public function endTest() {
+        unset($this->Trend);
+        ClassRegistry::flush();
+    }
+
+    // =========================================================================
+    public function test_trends() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('trends');
+        $this->assertIdentical($this->Trend->request['uri']['path'], 'trends');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+
+    // =========================================================================
+    public function test_current() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('current');
+        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/current');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+
+    // =========================================================================
+    public function test_daily() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('daily');
+        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/daily');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+
+    // =========================================================================
+    public function test_weekly() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('weekly');
+        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/weekly');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+    // =========================================================================
+    public function test_available() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('available');
+        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/available');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+    // =========================================================================
+    public function test_woeid() {
+        $this->Trend->getDataSource()->expectOnce('request');
+        $this->Trend->find('woeid', array('woeid' => 1));
+        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/1');
+        $this->assertIdentical($this->Trend->request['uri']['query'], array());
+    }
+
+    // =========================================================================
+    public function test_trends_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('trends');
+        $this->assertNotNull($datas['trends']);
+        $this->assertNotNull($datas['as_of']);
+    }
+
+    // =========================================================================
+    public function test_current_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('current');
+        $this->assertNotNull($datas['trends']);
+        $this->assertNotNull($datas['as_of']);
+    }
+
+    // =========================================================================
+    public function test_daily_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('daily');
+        $this->assertNotNull($datas['trends']);
+        $this->assertNotNull($datas['as_of']);
+    }
+
+    // =========================================================================
+    public function test_weekly_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('weekly');
+        $this->assertNotNull($datas['trends']);
+        $this->assertNotNull($datas['as_of']);
+    }
+    // =========================================================================
+    public function test_available_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('available', array());
+        $this->assertNotNull($datas);
+    }
+    // =========================================================================
+    public function test_woeid_real() {
+        $this->Trend = new TwimTrend();
+        $datas = $this->Trend->find('woeid', array('woeid' => 1));
+        $this->assertNotNull($datas['trends']);
+        $this->assertNotNull($datas['as_of']);
+        $this->assertNotNull($datas['locations']);
+    }
+
+}
