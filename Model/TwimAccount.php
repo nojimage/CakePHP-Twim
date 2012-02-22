f78057575e27d5a83d45c4ec9594ada0118cf7cb
nojimage <nojimage@gmail.com>
2012-02-22 11:36:10 +0900

    pass TwimAccount test

--- Model/TwimAccount.php
+++ Model/TwimAccount.php
@@ -5,14 +5,14 @@
  *
  * PHP versions 5
  *
- * Copyright 2011, nojimage (http://php-tips.com/)
+ * Copyright 2012, nojimage (http://php-tips.com/)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @version   1.0
+ * @version   2.0
  * @author    nojimage <nojimage at gmail.com>
- * @copyright 2011 nojimage (http://php-tips.com/)
+ * @copyright 2012 nojimage (http://php-tips.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @package   twim
  * @since   　File available since Release 1.0
@@ -30,118 +30,118 @@
  * @todo support post methods
  * @todo find method (verifyCredentials. totals, settings) not test yet.
  */
+App::uses('TwimAppModel', 'Twim.Model');
+
+/**
+ * 
+ */
 class TwimAccount extends TwimAppModel {
 
-    public $apiUrlBase = '1/account/';
+	public $apiUrlBase = '1/account/';
 
-    /**
-     * Custom find types available on this model
-     *
-     * @var array
-     */
-    public $_findMethods = array(
-        'rateLimitStatus' => true,
-        'verifyCredentials' => true,
-        'totals' => true,
-        'settings' => true,
-    );
+	/**
+	 * Custom find types available on this model
+	 *
+	 * @var array
+	 */
+	public $_findMethods = array(
+		'rateLimitStatus' => true,
+		'verifyCredentials' => true,
+		'totals' => true,
+		'settings' => true,
+	);
 
-    /**
-     * The custom find types that require authentication
-     *
-     * @var array
-     */
-    public $findMethodsRequiringAuth = array(
-        'verifyCredentials',
-        'totals',
-        'settings',
-    );
+	/**
+	 * The custom find types that require authentication
+	 *
+	 * @var array
+	 */
+	public $findMethodsRequiringAuth = array(
+		'verifyCredentials',
+		'totals',
+		'settings',
+	);
 
-    /**
-     * The options allowed by each of the custom find types
-     * 
-     * @var array
-     */
-    public $allowedFindOptions = array(
-        'rateLimitStatus' => array(),
-        'verifyCredentials' => array('include_entities', 'skip_status'),
-        'totals' => array(),
-        'settings' => array(),
-    );
+	/**
+	 * The options allowed by each of the custom find types
+	 * 
+	 * @var array
+	 */
+	public $allowedFindOptions = array(
+		'rateLimitStatus' => array(),
+		'verifyCredentials' => array('include_entities', 'skip_status'),
+		'totals' => array(),
+		'settings' => array(),
+	);
 
-    /**
-     * api limit data
-     *
-     * @var array
-     */
-    protected $_limit = array(
-        'hourly_limit' => null,
-        'reset_time_in_seconds' => null,
-        'reset_time' => null,
-        'remaining_hits' => null,
-    );
+	/**
+	 * api limit data
+	 *
+	 * @var array
+	 */
+	protected $_limit = array(
+		'hourly_limit' => null,
+		'reset_time_in_seconds' => null,
+		'reset_time' => null,
+		'remaining_hits' => null,
+	);
 
-    /**
-     * The vast majority of the custom find types actually follow the same format
-     * so there was little point explicitly writing them all out. Instead, if the
-     * method corresponding to the custom find type doesn't exist, the options are
-     * applied to the model's request property here and then we just call
-     * parent::find('all') to actually trigger the request and return the response
-     * from the API.
-     *
-     * In addition, if you try to fetch a timeline that supports paging, but you
-     * don't specify paging params, you really want all tweets in that timeline
-     * since time imemoriam. But twitter will only return a maximum of 200 per
-     * request. So, we make multiple calls to the API for 200 tweets at a go, for
-     * subsequent pages, then merge the results together before returning them.
-     *
-     * Twitter's API uses a count parameter where in CakePHP we'd normally use
-     * limit, so we also copy the limit value to count so we can use our familiar
-     * params.
-     *
-     * @param string $type
-     * @param array $options
-     * @return mixed
-     */
-    public function find($type, $options = array()) {
+	/**
+	 * The vast majority of the custom find types actually follow the same format
+	 * so there was little point explicitly writing them all out. Instead, if the
+	 * method corresponding to the custom find type doesn't exist, the options are
+	 * applied to the model's request property here and then we just call
+	 * parent::find('all') to actually trigger the request and return the response
+	 * from the API.
+	 *
+	 * In addition, if you try to fetch a timeline that supports paging, but you
+	 * don't specify paging params, you really want all tweets in that timeline
+	 * since time imemoriam. But twitter will only return a maximum of 200 per
+	 * request. So, we make multiple calls to the API for 200 tweets at a go, for
+	 * subsequent pages, then merge the results together before returning them.
+	 *
+	 * Twitter's API uses a count parameter where in CakePHP we'd normally use
+	 * limit, so we also copy the limit value to count so we can use our familiar
+	 * params.
+	 *
+	 * @param string $type
+	 * @param array $options
+	 * @return mixed
+	 */
+	public function find($type, $options = array()) {
+		if (method_exists($this, '_find' . Inflector::camelize($type))) {
+			return parent::find($type, $options);
+		}
 
-        if (method_exists($this, '_find' . Inflector::camelize($type))) {
-            return parent::find($type, $options);
-        }
+		$this->_setupRequest($type, $options);
 
-        $this->_setupRequest($type, $options);
+		return parent::find('all', $options);
+	}
 
-        return parent::find('all', $options);
-    }
+	/**
+	 * get api call remaining
+	 * 
+	 * @package bool $refresh
+	 * @return bool
+	 */
+	public function getApiRemain($refresh = true) {
+		if ($refresh || empty($this->_limit)) {
+			$this->_limit = $this->find('rateLimitStatus');
+		}
+		return $this->_limit['remaining_hits'];
+	}
 
-    /**
-     * get api call remaining
-     * 
-     * @package bool $refresh
-     * @return bool
-     */
-    public function getApiRemain($refresh = true) {
-
-        if ($refresh || empty($this->_limit)) {
-            $this->_limit = $this->find('rateLimitStatus');
-        }
-
-        return $this->_limit['remaining_hits'];
-    }
-
-    /**
-     * get api limit refresh time (sec)
-     * 
-     * @package bool $refresh
-     * @return int
-     */
-    public function getApiResetTime($refresh = true) {
-
-        if ($refresh || empty($this->_limit)) {
-            $this->_limit = $this->find('rateLimitStatus');
-        }
-
-        return $this->_limit['reset_time_in_seconds'];
-    }
+	/**
+	 * get api limit refresh time (sec)
+	 * 
+	 * @package bool $refresh
+	 * @return int
+	 */
+	public function getApiResetTime($refresh = true) {
+		if ($refresh || empty($this->_limit)) {
+			$this->_limit = $this->find('rateLimitStatus');
+		}
+		return $this->_limit['reset_time_in_seconds'];
+	}
 
 }
