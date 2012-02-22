973b15c66bbf976736d253663717fa7665120440
nojimage <nojimage@gmail.com>
2012-02-22 12:29:59 +0900

    pass TwimTrend test

--- Model/TwimTrend.php
+++ Model/TwimTrend.php
@@ -5,14 +5,14 @@
  *
  * PHP versions 5
  *
- * Copyright 2011, nojimage (http://php-tips.com/)
+ * Copyright 2012, nojimage (http://php-tips.com/)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @version   1.0
+ * @version   2.0
  * @author    nojimage <nojimage at gmail.com>
- * @copyright 2011 nojimage (http://php-tips.com/)
+ * @copyright 2012 nojimage (http://php-tips.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @package   twim
  * @since   　File available since Release 1.0
@@ -22,80 +22,84 @@
  * @link      http://dev.twitter.com/doc/get/trends/1
  *
  */
+App::uses('TwimAppModel', 'Twim.Model');
+
+/**
+ *
+ */
 class TwimTrend extends TwimAppModel {
 
-    public $apiUrlBase = '1/trends/';
+	public $apiUrlBase = '1/trends/';
 
-    /**
-     * Custom find types available on this model
-     *
-     * @var array
-     */
-    public $findMethods = array(
-        'daily' => true,
-        'weekly' => true,
-        'available' => true,
-        'woeid' => true,
-    );
+	/**
+	 * Custom find types available on this model
+	 *
+	 * @var array
+	 */
+	public $findMethods = array(
+		'daily' => true,
+		'weekly' => true,
+		'available' => true,
+		'woeid' => true,
+	);
 
-    /**
-     * The options allowed by each of the custom find types
-     *
-     * @var array
-     */
-    public $allowedFindOptions = array(
-        'daily' => array('exclude', 'date'),
-        'weekly' => array('exclude', 'date'),
-        'available' => array('lat', 'long'),
-        'woeid' => array('woeid'),
-    );
+	/**
+	 * The options allowed by each of the custom find types
+	 *
+	 * @var array
+	 */
+	public $allowedFindOptions = array(
+		'daily' => array('exclude', 'date'),
+		'weekly' => array('exclude', 'date'),
+		'available' => array('lat', 'long'),
+		'woeid' => array('woeid'),
+	);
 
-    /**
-     *
-     * @param mixed $type
-     * @param array $options
-     * @return array|false
-     */
-    public function find($type, $options = array()) {
+	/**
+	 *
+	 * @param mixed $type
+	 * @param array $options
+	 * @return array|false
+	 */
+	public function find($type, $options = array()) {
+		if (method_exists($this, '_find' . Inflector::camelize($type))) {
+			return parent::find($type, $options);
+		}
 
-        if (method_exists($this, '_find' . Inflector::camelize($type))) {
-            return parent::find($type, $options);
-        }
+		$this->_setupRequest($type, $options);
 
-        $this->_setupRequest($type, $options);
+		if ($type === 'trends') {
+			$this->request['uri']['path'] = Inflector::underscore($type);
+		}
 
-        if ($type === 'trends') {
-            $this->request['uri']['path'] = Inflector::underscore($type);
-        }
+		return parent::find('all', $options);
+	}
 
-        return parent::find('all', $options);
-    }
+	/**
+	 *
+	 * @param string $state
+	 * @param array $query
+	 * @param array $results
+	 * @return mixed
+	 */
+	protected function _findWoeid($state, $query = array(), $results = array()) {
+		if ($state === 'before') {
+			if (empty($query['woeid'])) {
+				return $query;
+			}
 
-    /**
-     *
-     * @param string $state
-     * @param array $query
-     * @param array $results
-     * @return mixed
-     */
-    protected function _findWoeid($state, $query = array(), $results = array()) {
-        if ($state === 'before') {
-            if (empty($query['woeid'])) {
-                return $query;
-            }
+			$this->_setupRequest('woeid', $query);
 
-            $this->_setupRequest('woeid', $query);
+			$this->request['uri']['path'] = $this->apiUrlBase . $query['woeid'];
+			unset($query['woeid']);
+			unset($this->request['uri']['query']['woeid']);
 
-            $this->request['uri']['path'] = $this->apiUrlBase . $query['woeid'];
-            unset($query['woeid']);
-            unset($this->request['uri']['query']['woeid']);
-
-            return $query;
-        } else if ($state === 'after') {
-            return!empty($results[0]) ? $results[0] : $results;
-        } else {
-            return $results;
-        }
-    }
+			return $query;
+		} else if ($state === 'after') {
+			return !empty($results[0]) ? $results[0] : $results;
+		} else {
+			return $results;
+		}
+	}
 
 }
--- Test/Case/Model/TwimTrendTest.php
+++ Test/Case/Model/TwimTrendTest.php
@@ -5,22 +5,21 @@
  *
  * PHP versions 5
  *
- * Copyright 2011, nojimage (http://php-tips.com/)
+ * Copyright 2012, nojimage (http://php-tips.com/)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * @filesource
- * @version   1.0
+ * @version   2.0
  * @author    nojimage <nojimage at gmail.com>
- * @copyright 2011 nojimage (http://php-tips.com/)
+ * @copyright 2012 nojimage (http://php-tips.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @package   twim
  * @since   　File available since Release 1.0
  *
  */
-App::import('Lib', 'Twim.TwimConnectionTestCase');
-App::import('Model', 'Twim.TwimTrend');
+App::uses('TwimConnectionTestCase', 'Twim.TestSuite');
 
 /**
  *
@@ -28,78 +27,87 @@ App::import('Model', 'Twim.TwimTrend');
  */
 class TwimTrendTestCase extends TwimConnectionTestCase {
 
-    public function startTest() {
-        $this->Trend = ClassRegistry::init('Twim.TwimTrend');
-        $this->Trend->setDataSource($this->mockDatasourceName);
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->Trend = ClassRegistry::init('Twim.TwimTrend');
+		$this->Trend->setDataSource($this->mockDatasourceName);
+	}
 
-    public function endTest() {
-        unset($this->Trend);
-        ClassRegistry::flush();
-    }
+	public function tearDown() {
+		unset($this->Trend);
+		parent::tearDown();
+	}
 
-    // =========================================================================
-    public function test_daily() {
-        $this->Trend->getDataSource()->expectOnce('request');
-        $this->Trend->find('daily');
-        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/daily');
-        $this->assertIdentical($this->Trend->request['uri']['query'], array());
-    }
+	// =========================================================================
 
-    // =========================================================================
-    public function test_weekly() {
-        $this->Trend->getDataSource()->expectOnce('request');
-        $this->Trend->find('weekly');
-        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/weekly');
-        $this->assertIdentical($this->Trend->request['uri']['query'], array());
-    }
+	public function testDaily() {
+		$this->Trend->getDataSource()->expects($this->once())->method('request')->will($this->returnValue(array()));
+		$this->Trend->find('daily');
+		$this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/daily');
+		$this->assertIdentical($this->Trend->request['uri']['query'], array());
+	}
 
-    // =========================================================================
-    public function test_available() {
-        $this->Trend->getDataSource()->expectOnce('request');
-        $this->Trend->find('available');
-        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/available');
-        $this->assertIdentical($this->Trend->request['uri']['query'], array());
-    }
+	// =========================================================================
 
-    // =========================================================================
-    public function test_woeid() {
-        $this->Trend->getDataSource()->expectOnce('request');
-        $this->Trend->find('woeid', array('woeid' => 1));
-        $this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/1');
-        $this->assertIdentical($this->Trend->request['uri']['query'], array());
-    }
+	public function testWeekly() {
+		$this->Trend->getDataSource()->expects($this->once())->method('request')->will($this->returnValue(array()));
+		$this->Trend->find('weekly');
+		$this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/weekly');
+		$this->assertIdentical($this->Trend->request['uri']['query'], array());
+	}
 
-    // =========================================================================
-    public function test_daily_real() {
-        $this->Trend->setDataSource($this->testDatasourceName);
-        $datas = $this->Trend->find('daily');
-        $this->assertNotNull($datas['trends']);
-        $this->assertNotNull($datas['as_of']);
-    }
+	// =========================================================================
 
-    // =========================================================================
-    public function test_weekly_real() {
-        $this->Trend->setDataSource($this->testDatasourceName);
-        $datas = $this->Trend->find('weekly');
-        $this->assertNotNull($datas['trends']);
-        $this->assertNotNull($datas['as_of']);
-    }
+	public function testAvailable() {
+		$this->Trend->getDataSource()->expects($this->once())->method('request')->will($this->returnValue(array()));
+		$this->Trend->find('available');
+		$this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/available');
+		$this->assertIdentical($this->Trend->request['uri']['query'], array());
+	}
 
-    // =========================================================================
-    public function test_available_real() {
-        $this->Trend->setDataSource($this->testDatasourceName);
-        $datas = $this->Trend->find('available', array());
-        $this->assertNotNull($datas);
-    }
+	// =========================================================================
 
-    // =========================================================================
-    public function test_woeid_real() {
-        $this->Trend->setDataSource($this->testDatasourceName);
-        $datas = $this->Trend->find('woeid', array('woeid' => 1));
-        $this->assertNotNull($datas['trends']);
-        $this->assertNotNull($datas['as_of']);
-        $this->assertNotNull($datas['locations']);
-    }
+	public function testWoeid() {
+		$this->Trend->getDataSource()->expects($this->once())->method('request')->will($this->returnValue(array()));
+		$this->Trend->find('woeid', array('woeid' => 1));
+		$this->assertIdentical($this->Trend->request['uri']['path'], '1/trends/1');
+		$this->assertIdentical($this->Trend->request['uri']['query'], array());
+	}
+
+	// =========================================================================
+
+	public function testDaily_real() {
+		$this->Trend->setDataSource($this->testDatasourceName);
+		$datas = $this->Trend->find('daily');
+		$this->assertNotNull($datas['trends']);
+		$this->assertNotNull($datas['as_of']);
+	}
+
+	// =========================================================================
+
+	public function testWeekly_real() {
+		$this->Trend->setDataSource($this->testDatasourceName);
+		$datas = $this->Trend->find('weekly');
+		$this->assertNotNull($datas['trends']);
+		$this->assertNotNull($datas['as_of']);
+	}
+
+	// =========================================================================
+
+	public function testAvailable_real() {
+		$this->Trend->setDataSource($this->testDatasourceName);
+		$datas = $this->Trend->find('available', array());
+		$this->assertNotNull($datas);
+	}
+
+	// =========================================================================
+
+	public function testWoeid_real() {
+		$this->Trend->setDataSource($this->testDatasourceName);
+		$datas = $this->Trend->find('woeid', array('woeid' => 1));
+		$this->assertNotNull($datas['trends']);
+		$this->assertNotNull($datas['as_of']);
+		$this->assertNotNull($datas['locations']);
+	}
 
 }
