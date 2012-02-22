683cb264fe5f16ebbb01d117522770f24b6e421a
nojimage <nojimage@gmail.com>
2012-02-22 11:31:58 +0900

    pass Twim test

--- Model/Twim.php
+++ Model/Twim.php
@@ -1,16 +1,18 @@
 <?php
 
+App::uses('TwimAppModel', 'Twim.Model');
+
 /**
  * alias of TwimAppModel
  *
- * @version   1.0
+ * @version   2.0
  * @author    nojimage <nojimage at gmail.com>
- * @copyright 2011 nojimage (http://php-tips.com/)
+ * @copyright 2012 nojimage (http://php-tips.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link      http://php-tips.com/
  * @package   twim
  * @since     File available since Release 1.0
  */
 class Twim extends TwimAppModel {
-    
+	//
 }
--- Test/Case/Model/TwimTest.php
+++ Test/Case/Model/TwimTest.php
@@ -1,7 +1,6 @@
 <?php
 
-App::import('Lib', 'Twim.TwimConnectionTestCase');
-App::import('Model', array('Twim.Twim'));
+App::uses('TwimConnectionTestCase', 'Twim.TestSuite');
 
 /**
  *
@@ -9,22 +8,22 @@ App::import('Model', array('Twim.Twim'));
  */
 class TwimTestCase extends TwimConnectionTestCase {
 
-    public function startTest($method) {
-        $this->Twim = ClassRegistry::init('Twim');
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->Twim = ClassRegistry::init('Twim.Twim');
+	}
 
-    public function endTest($method) {
-        unset($this->Twim);
-        ClassRegistry::flush();
-    }
+	public function tearDown() {
+		unset($this->Twim);
+		parent::tearDown();
+	}
 
-    // =========================================================================
-    public function test_serach() {
-        $q = 'test';
-        $limit = 100;
-        $datas = $this->Twim->Search->find('search', compact('q', 'limit'));
-        $this->assertTrue(!empty($datas));
-        $this->assertIdentical(count($datas), 100);
-    }
+	public function testSerach() {
+		$q = 'test';
+		$limit = 100;
+		$datas = $this->Twim->Search->find('search', compact('q', 'limit'));
+		$this->assertTrue(!empty($datas));
+		$this->assertIdentical(count($datas), 100);
+	}
 
 }
