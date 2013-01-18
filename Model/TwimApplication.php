e876d2cc67af2a5de6259d05cebe1cfd68c1d067
nojimage <nojimage@gmail.com>
2013-01-18 19:56:05 +0900

    update version number

--- Model/TwimApplication.php
+++ Model/TwimApplication.php
@@ -3,17 +3,17 @@
 /**
  * for Application API
  *
- * CakePHP 2.0
+ * CakePHP 2.x
  * PHP version 5
  *
- * Copyright 2012, nojimage (http://php-tips.com/)
+ * Copyright 2013, nojimage (http://php-tips.com/)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @version   2.0
+ * @version   2.1
  * @author    nojimage <nojimage at gmail.com>
- * @copyright 2012 nojimage (http://php-tips.com/)
+ * @copyright 2013 nojimage (http://php-tips.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  * @package   Twim
  * @since     File available since Release 1.0
